c2794848325a6220674346a3b129fab5fabe065f
Anagha Antu <161487886+anaghaantu@users.noreply.github.com>
2025-07-02 19:21:07 +0530

    primenumber in a range.py

---
+++ primenumber in a range.py
@@ -0,0 +1,16 @@
+low = int(input("enter the lower limit:"))
+high = int(input("enter the higher limit:"))
+primes =[]
+for i in range (low,high+1):
+    flag =0
+    if i<2:
+        continue
+    if i==2:
+        primes.append(2)
+    for x in range(2,i):
+        if i % x==0:
+            flag =1
+            break
+    if flag==0:
+            primes.append(i)
+print(primes)
