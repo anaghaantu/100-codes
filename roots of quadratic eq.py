0df00bef5f2b7671f98657ab2cbb76a5591dce1b
Anagha Antu <161487886+anaghaantu@users.noreply.github.com>
2025-07-21 04:46:40 +0530

    Create roots of quadratic eq.py

---
+++ roots of quadratic eq.py
@@ -0,0 +1,7 @@
+a=int(input("enter x^2"))
+b=int(input("enter x"))
+c=int(input("enter constant"))
+x1 = (-b + (b ** 2 -4 * a * c) ** 0.5)/ 2 * a
+x2 = (-b -(b ** 2 -4 * a * c) ** 0.5)/2 * a
+print(x1)
+print(x2)
